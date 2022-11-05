d663a5d1a545eafc4e009b309a5155d56f652bf4
nkubito-pacis <pacisnkubito@gmail.com>
2022-11-05 07:36:14 +0200

    Create StickyNotesApplication.java

---
+++ src/main/java/rw/stickynotes/StickyNotesApplication.java
@@ -0,0 +1,17 @@
+package rw.stickynotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableAsync
+@EnableCaching
+public class StickyNotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StickyNotesApplication.class, args);
+    }
+
+}
