0383ef9a28ab51262489795341ef4ea46bee8c4e
nkubito-pacis <pacisnkubito@gmail.com>
2022-11-18 07:38:47 +0200

    Create StickyNotesApplicationTests.java

---
+++ src/test/java/rw/stickynotes/StickyNotesApplicationTests.java
@@ -0,0 +1,15 @@
+package rw.stickynotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class StickyNotesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
